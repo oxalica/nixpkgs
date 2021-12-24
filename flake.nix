6491cbe1504dd4c0c47dffa5b61f9f751dc12a37
oxalica <oxalicc@pm.me>
2021-12-25 02:46:57 +0800

    flake.nix: add riscv64-linux

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
 
       lib = import ./lib;
 
-      systems = lib.systems.supported.hydra;
+      systems = lib.systems.supported.hydra ++ [ "riscv64-linux" ];
 
       forAllSystems = f: lib.genAttrs systems (system: f system);
 
