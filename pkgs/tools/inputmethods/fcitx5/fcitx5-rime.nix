7c84ebd513795a29aafb63af743dccbe73adc8eb
oxalica <oxalicc@pm.me>
2022-06-23 07:02:19 +0800

    fcitx5-rime: package rime-data together as default schemas like ibus-rime
    It provides schema settings including Pinyin and Wubi as a sane
    default for users, to eliminate the necessity to write them from scratch.
    This matches the behavior of ibus-rime, which is an official project
    under rime.
    
    Note that these schemas are just the default settings. If users don't
    import them, or have their own settings or overrides under
    `$XDG_DATA_HOME/fcitx5/rime`, user settings are always preferred over
    rime-data.

--- pkgs/tools/inputmethods/fcitx5/fcitx5-rime.nix
+++ pkgs/tools/inputmethods/fcitx5/fcitx5-rime.nix
@@ -6,6 +6,7 @@
 , gettext
 , fcitx5
 , librime
+, rime-data
 }:
 
 stdenv.mkDerivation rec {
@@ -19,9 +20,13 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-/oQdBCDV5obSHw7dzdceC+zWHcNve3NDlA50GhvkK8o=";
   };
 
-  cmakeFlags = [
-    "-DRIME_DATA_DIR=${placeholder "out"}/share/rime-data"
-  ];
+  cmakeFlags = [ "-DRIME_DATA_DIR=${rime-data}/share/rime-data" ];
+
+  prePatch = ''
+    substituteInPlace data/CMakeLists.txt \
+       --replace 'DESTINATION "''${RIME_DATA_DIR}"' \
+                 'DESTINATION "''${CMAKE_INSTALL_DATADIR}/rime-data"'
+  '';
 
   nativeBuildInputs = [
     cmake
